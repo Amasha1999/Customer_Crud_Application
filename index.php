b9e41df4b47abbc0b99585ed13cb5de9719a1728
Amasha1999 <96760573+Amasha1999@users.noreply.github.com>
2024-01-30 21:27:19 +0530

    update index.php

--- index.php
+++ index.php
@@ -1,5 +1,3 @@
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -25,46 +23,55 @@
     𝐂𝐮𝐬𝐭𝐨𝐦𝐞𝐫 𝐂𝐫𝐮𝐝 𝐀𝐩𝐩𝐥𝐢𝐜𝐚𝐭𝐢𝐨𝐧
 </nav>
 <div class="container">
+  
     <a href="add_new.php" class="btn btn-dark mb-3">Add New </a>
     
     <table class="table table-hover text-center">
   <thead class="table-dark">
     <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
+      <th scope="col">ID</th>
+      <th scope="col">First Name</th>
+      <th scope="col">Last Name</th>
+      <th scope="col">Email</th>
+      <th scope="col">Gender</th>
+      <th scope="col">Action</th>
     </tr>
   </thead>
   <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td colspan="2">Larry the Bird</td>
-      <td>@twitter</td>
-    </tr>
+    <?php
+    include "db_conn.php";
+    $sql = "SELECT * FROM `crud`";
+    $result = mysqli_query($conn, $sql);
+    while ($row = mysqli_fetch_assoc($result)) {
+      ?>
+       <tr>
+                  <td> <?php echo $row['id']?> </td>
+                  <td> <?php echo $row['first_name']?> </td>
+                  <td> <?php echo $row['last_name']?> </td>
+                  <td> <?php echo $row['email']?> </td>
+                  <td> <?php echo $row['gender']?> </td>
+
+                  
+                    
+                    <td>
+                      <a href="edit.php?id=<?php echo $row['id'] ?>"
+                         class="btn btn-success btn-sm">Edit</a>
+
+                         <a href="delete.php?id=<?php echo $row['id'] ?>"
+                        class="btn btn-danger btn-sm">Delete</a>
+                    </td>
+                    
+        </tr>
+
+      <?php
+    }
+    ?>
   </tbody>
 </table>
-    
-
 </div>
 
-  
-
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.
-bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" 
-crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" 
+    integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" 
+    crossorigin="anonymous"></script>
 </body>
 </html>
